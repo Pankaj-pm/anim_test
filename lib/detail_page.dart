aa2e8d484911cad474c1599a5649cb143d9fbe6b
AnandDL02 <ananddalsaniya2002@gmail.com>
2024-06-24 09:45:47 +0530

    init commit

---
+++ lib/detail_page.dart
@@ -0,0 +1,176 @@
+import 'dart:math';
+
+import 'dart:math';
+
+import 'package:anim_test/main.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  final int index;
+
+  const DetailPage({super.key, required this.index});
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  ScrollController controller = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Text"),
+        actions: [
+          TextButton(onPressed: () {
+            controller.animateTo(
+              0,
+              duration: Duration(seconds: 3),
+              curve: Curves.easeOutBack,
+            );
+          }, child: Text("Home")),
+          TextButton(onPressed: () {
+            controller.animateTo(
+              500,
+              duration: Duration(seconds: 3),
+              curve: Curves.easeOutBack,
+            );
+          }, child: Text("About")),
+          TextButton(onPressed: () {
+            controller.animateTo(
+              1000,
+              duration: Duration(seconds: 3),
+              curve: Curves.easeOutBack,
+            );
+          }, child: Text("Contact")),
+          Hero(
+            tag: "icon${widget.index}",
+            child: Icon(
+              Icons.flight_takeoff,
+              size: 40,
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        controller: controller,
+        child: Column(
+          children: [
+            Container(
+              height: 500,
+              width: double.infinity,
+              color: Colors.black12,
+              margin: EdgeInsets.all(5),
+              child: Text("Home", style: TextStyle(fontSize: 50)),
+            ),
+            Container(
+              height: 500,
+              width: double.infinity,
+              color: Colors.black12,
+              margin: EdgeInsets.all(5),
+              child: Text("About", style: TextStyle(fontSize: 50)),
+            ),
+            Container(
+              height: 500,
+              width: double.infinity,
+              color: Colors.black12,
+              margin: EdgeInsets.all(5),
+              child: Text("Contact", style: TextStyle(fontSize: 50)),
+            ),
+            InkWell(
+              onTap: () {
+                setState(() {});
+              },
+              onHover: (value) {
+                print("object");
+                if (value) {
+                  setState(() {});
+                }
+              },
+              child: AnimatedContainer(
+                width: Random().nextInt(200) + 20.toDouble(),
+                height: Random().nextInt(200) + 20.toDouble(),
+                duration: const Duration(milliseconds: 500),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, Random().nextInt(255), Random().nextInt(255), Random().nextInt(255)),
+                    border: Border.all(
+                      color: Color.fromARGB(255, Random().nextInt(255), Random().nextInt(255), Random().nextInt(255)),
+                      width: Random().nextInt(5).toDouble(),
+                    ),
+                    borderRadius: BorderRadius.circular(Random().nextInt(50).toDouble())),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                setState(() {});
+              },
+              onHover: (value) {
+                print("object");
+                if (value) {
+                  setState(() {});
+                }
+              },
+              child: AnimatedContainer(
+                width: Random().nextInt(200) + 20.toDouble(),
+                height: Random().nextInt(200) + 20.toDouble(),
+                duration: const Duration(milliseconds: 500),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, Random().nextInt(255), Random().nextInt(255), Random().nextInt(255)),
+                    border: Border.all(
+                      color: Color.fromARGB(255, Random().nextInt(255), Random().nextInt(255), Random().nextInt(255)),
+                      width: Random().nextInt(5).toDouble(),
+                    ),
+                    borderRadius: BorderRadius.circular(Random().nextInt(50).toDouble())),
+              ),
+            ),
+            ListView.builder(
+              itemCount: 60,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return Container(
+                  width: double.infinity,
+                  color: Colors.blue,
+                  margin: EdgeInsets.all(10),
+                  child: Text("index $index"),
+                );
+              },
+            ),
+            AnimatedContainer(
+              width: 300,
+              height: 300,
+              margin: EdgeInsets.all(10),
+              duration: const Duration(milliseconds: 500),
+              child: Hero(
+                tag: "img${widget.index}",
+                child: Image.network(
+                  playlist[widget.index],
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            FloatingActionButton(
+                heroTag: "h1",
+                onPressed: () {
+                  setState(() {});
+                },
+                child: Icon(Icons.add)),
+            FloatingActionButton(onPressed: () {}, child: Icon(Icons.remove)),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // controller.jumpTo(controller.position.maxScrollExtent);
+          controller.animateTo(
+            controller.position.maxScrollExtent,
+            duration: Duration(seconds: 3),
+            curve: Curves.easeOutBack,
+          );
+        },
+        child: Icon(Icons.download),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:anim_test/detail_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: playlist.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return DetailPage(
+                    index: index,
+                  );
+                },
+              ));
+            },
+            child: Column(
+              children: [
+                Container(
+                  height: 200,
+                  margin: EdgeInsets.all(10),
+                  child: Hero(
+                    tag: "img$index",
+                    child: Image.network(playlist[index]),
+                  ),
+                ),
+                Hero(
+                  tag: "icon$index",
+                  placeholderBuilder: (context, heroSize, child) {
+                    return Icon(Icons.air, size: 40);
+                  },
+                  flightShuttleBuilder: (flightContext, animation, flightDirection, fromHeroContext, toHeroContext) {
+                    return Icon(Icons.flight_takeoff, size: 40);
+                  },
+                  child: Icon(
+                    Icons.flight,
+                    size: 40,
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return DetailPage(
+                    index: 0,
+                  );
+                },
+              ));
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.remove),
+          ),
+          FloatingActionButton(
+            heroTag: "h1",
+            onPressed: () {},
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+final List<String> playlist = [
+  'https://a10.gaanacdn.com/gn_img/albums/w4MKPObojg/MKPDB1YAKo/size_l.jpg',
+  'https://a10.gaanacdn.com/gn_img/albums/Rz4W8vKxD5/4W87PAOO3x/size_l.jpg',
+  'https://a10.gaanacdn.com/gn_img/albums/4Z9bqo3yQn/9bqZ41LXKy/size_l.jpg',
+  'https://a10.gaanacdn.com/gn_img/albums/D0PKLrWGl9/0PKLDnzwKG/size_l_1581694054.webp',
+  'https://a10.gaanacdn.com/gn_img/albums/ZaP37RKDy7/P37O6Rql3D/size_l.jpg',
+  'https://cdn.prod.website-files.com/5f841209f4e71b2d70034471/60bb4a2e143f632da3e56aea_Flutter%20app%20development%20(2).png',
+];
